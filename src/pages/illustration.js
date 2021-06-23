eac5f681a48e06999c37a0ef6c5647291be13592
Vi-Anh Nguyen <41245033+bizbunny@users.noreply.github.com>
2021-06-22 21:28:06 -0500

    Create illustration.js

---
+++ src/pages/illustration.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles.css";
+import { Typography, Container, Grid } from "@material-ui/core";
+export default function Illustration() {
+  return (
+    <Container>
+      <Grid container>
+        <Grid item>
+          <Typography>Illustration</Typography>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+}
