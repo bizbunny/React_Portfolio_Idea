65157bf667c78515e2818fc14c9c08705f928c37
Vi-Anh Nguyen <41245033+bizbunny@users.noreply.github.com>
2021-06-22 21:22:47 -0500

    Create codeCards.js

---
+++ src/components/codeCards.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 340
+  },
+  media: {
+    height: 140
+  }
+});
+
+export default function CodeCard({
+  image,
+  title,
+  codetitle,
+  description,
+  link
+}) {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia className={classes.media} image={image} title={title} />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {codetitle}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          <a href={link}>Learn More</a>
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
