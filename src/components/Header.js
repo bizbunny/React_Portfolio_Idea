3e38cb7a63b0cc08c61d3231d81e37f44063c086
Vi-Anh Nguyen <41245033+bizbunny@users.noreply.github.com>
2021-06-22 21:19:18 -0500

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "../styles.css";
+export default function Header() {
+  return (
+    <React.Fragment>
+      <header className="header">Vi-Anh Nguyen</header>
+      <header className="smallheader">Art and Code</header>
+    </React.Fragment>
+  );
+}
