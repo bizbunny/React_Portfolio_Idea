791542769a75d622cfb6f301f472da3c682f4a62
Vi-Anh Nguyen <41245033+bizbunny@users.noreply.github.com>
2021-06-22 21:25:23 -0500

    Create characterart.js

---
+++ src/pages/characterart.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Slider from "react-slick";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import "../styles.css";
+import { Typography, Container, Grid } from "@material-ui/core";
+export default function Characterart() {
+  return (
+    <Container>
+      <Grid container>
+        <Grid item>
+          <Typography>Character Art</Typography>
+          {/*slider*/}
+          {/*Sophie Art BEGIN*/}
+
+          {/*Sophie Art END*/}
+        </Grid>
+      </Grid>
+    </Container>
+  );
+}
